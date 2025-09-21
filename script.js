21ded09b98a99eb0162073bf368a1e093e5db964
Patrick McMahon <patrick.mcmahon@rmit.edu.au>
2025-09-21 18:21:45 +1000

    added tutorial

---
+++ script.js
@@ -0,0 +1,22 @@
+/* find the thing to animate */
+let circle = document.getElementById("circleOne");
+/* find the buttons and add eventListener */
+let aBOne = document.getElementById("animButtonOne");
+aBOne.addEventListener("click", animateOne);
+let aBTwo = document.getElementById("animButtonTwo");
+aBTwo.addEventListener("click", animateTwo);
+let aBThree = document.getElementById("animButtonThree");
+aBThree.addEventListener("click", animateThree);
+
+function animateOne() {
+  //
+}
+
+/* https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API/Using_the_Web_Animations_API */
+function animateTwo() {
+  //
+}
+
+function animateFour() {
+  //
+}
